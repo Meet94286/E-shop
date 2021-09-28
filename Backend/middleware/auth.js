8e4a528d5b25cd09dbf3307a22489baa3af4b993
Meet94286 <mehtameet4321.mm@gmail.com>
2021-09-29 02:01:38 +0530

    user auth created

---
+++ Backend/middleware/auth.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  const token = req.headers['x-auth-token'] || req.headers["Authorization"];
+
+  if (!token) return res.status(401).send("Access denied. No token provided.");
+  try {
+    jwt.verify(token, "myprivatekey");
+   // console.log("verified");
+     next();
+  } catch (ex) {
+    res.status(400).send("Invalid token.");
+  }
+};
